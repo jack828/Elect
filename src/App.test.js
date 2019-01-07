2ac10ed12b036db9eb05f23874ec6d60da7e1e93
Jack Burgess <jackburgess124@gmail.com>
2019-01-07 23:16:35 +0000

    Import Coreui bc4532486396dbd2532a4f102952a725487fab18 https://github.com/coreui/coreui-free-react-admin-template

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import {shallow} from 'enzyme/build';
+import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
+
+it('mounts without crashing', () => {
+  const wrapper = shallow(<App />);
+  wrapper.unmount()
+});
