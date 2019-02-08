c9b29cf2761cf5ceb6928b84e0be26b942081b13
Jack Burgess <jackburgess124@gmail.com>
2019-02-08 13:46:03 +0000

    Split auth into it's own folder

--- server/services/database/init.js
+++ server/services/database/init.js
@@ -8,17 +8,25 @@ const init = (serviceLocator, done) => {
     done(new Error('You must provide a database URL in config.databaseUrl or in the environment variable MONGO_URL'))
   }
   const { database } = mongodbUri.parse(connectionUri)
-  MongoClient.connect(connectionUri, { native_parser: true }, (err, client) => {
-    if (err) {
-      serviceLocator.logger.error(err.message, err.stack)
-      return done(err)
+  MongoClient.connect(
+    connectionUri,
+    {
+      native_parser: true,
+      useNewUrlParser: true
+    },
+    (err, client) => {
+      if (err) {
+        serviceLocator.logger.error(err.message, err.stack)
+        return done(err)
+      }
+      const db = client.db(database)
+      serviceLocator.logger.info('Connected to', database)
+      serviceLocator.register('serviceDatabase', db)
+      serviceLocator.register('serviceDatabaseClient', client)
+      serviceLocator.register('persistence', persistenceFactory(serviceLocator))
+      done()
     }
-    const db = client.db(database)
-    serviceLocator.logger.info('Connected to', database)
-    serviceLocator.register('serviceDatabase', db)
-    serviceLocator.register('persistence', persistenceFactory(serviceLocator))
-    done()
-  })
+  )
 }
 
 module.exports = () => ({ database: init })
