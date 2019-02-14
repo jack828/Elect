45f24dd34d96570765c0c2b6b34a5c5909845900
Jack Burgess <jackburgess124@gmail.com>
2019-02-14 14:49:41 +0000

    Site: Implement websocket based request-response pattern

--- server/services/realtime/realtime/controller.js
+++ server/services/realtime/realtime/controller.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws')
-
+const hat = require('hat')
 
 module.exports = (serviceLocator) => {
   serviceLocator.logger.info('Init realtime server')
@@ -12,11 +12,44 @@ module.exports = (serviceLocator) => {
     console.log(wss.address())
     console.log(wss.url)
     wss.on('connection', (ws) => {
-      ws.on('message', (message) => {
-        console.log('received: %s', message)
+      ws.id = hat()
+      serviceLocator.logger.info('New connection', ws.id)
+
+      ws.on('message', (raw) => {
+        let parsed = null
+        try {
+          parsed = JSON.parse(raw)
+        } catch (e) {
+          serviceLocator.logger.warn('Invalid data', raw)
+        }
+        const { id, ...data } = parsed
+
+        serviceLocator.logger.info('Received', id, data)
+        Object.keys(data).map((key) => {
+          serviceLocator.logger.debug('Emitting', key, id, data[key])
+          if (id) {
+            console.log('binding once for id', id)
+            wss.once(id, (value) => {
+            console.log('id callback triggered!', id, value)
+              ws.send(JSON.stringify({ [id]: value }))
+            })
+          }
+          wss.emit(key, id, data[key])
+        })
       })
 
       // ws.send(JSON.stringify({ 'election:vote': { _id: 'abc1234', party: 'absc3939u90' } }))
+    })
+
+    wss.on('test', (id, data) => {
+      console.log('recieved `test`', { id, data })
+
+      // async something
+
+      if (id) {
+        console.log('emitting callback id', id)
+        wss.emit(id, { new: 'data' })
+      }
     })
   })
 }
--- src/lib/websocket.js
+++ src/lib/websocket.js
@@ -1,19 +1,34 @@
 import EventEmitter from 'events'
+import hat from 'hat'
 
 // TODO: debug logger
 class Websocket extends EventEmitter {
   constructor(url) {
     super()
     this.url = url
-    this.ws = new WebSocket('ws://localhost:8080')
+    this.ws = new WebSocket('ws://localhost:3003')
 
     // TODO: disconnect, reconnect, etc - add a banner or something
     this.ws.onmessage = this.onMessage.bind(this)
     this.ws.onopen = this.onOpen.bind(this)
   }
 
+  send(key, data, callback) {
+    // TODO promises
+    const id = callback && hat()
+    const stringifiedData = JSON.stringify({
+      id,
+      [key]: data
+    })
+    this.ws.send(stringifiedData)
+    this.once(id, (value) => {
+      callback(null, value)
+    })
+  }
+
   onMessage({ data: raw }) {
     const data = this.parse(raw)
+    console.log(data)
     Object.keys(data).map((key) => {
       const value = data[key]
       this.emit(key, value)
