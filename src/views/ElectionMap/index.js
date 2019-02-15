51705cffea3cda9cc89787a003e2dba21d2e3371
Jack Burgess <jackburgess124@gmail.com>
2019-02-15 14:07:48 +0000

    Site: Election Map tidy

--- src/views/ElectionMap/index.js
+++ src/views/ElectionMap/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import L from 'leaflet'
 
+// TODO: improve loading by using https://docs.mapbox.com/studio-manual/reference/datasets/
 import constituencyData from './constituencies.json'
 
 class ElectionMap extends Component {
   componentDidMount() {
-    if (this.rendered) return false
+    if (!this.map) this.renderMap()
+  }
 
-    const map = L.map('map').setView([ 54, 0 ], 6)
+  getColour(val) {
+    if (val > 1000) return '#800026'
+    if (val > 500) return '#BD0026'
+    if (val > 200) return '#E31A1C'
+    if (val > 100) return '#FC4E2A'
+    if (val > 50) return '#FD8D3C'
+    if (val > 20) return '#FEB24C'
+    if (val > 10) return '#FED976'
+    return '#FFEDA0'
+  }
+
+  renderMap() {
+    this.map = L.map('map').setView([ 54, -2 ], 6)
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFjazgyOCIsImEiOiJjanM1enVzdWkwMGszNGFwaGMwN3JnMmVoIn0.r9CfYSdy4rdGFBHO-MJt7w', {
       maxZoom: 18,
@@ -15,44 +29,22 @@ class ElectionMap extends Component {
         + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
         + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       id: 'mapbox.light'
-    }).addTo(map)
-
+    }).addTo(this.map)
 
     // control that shows state info on hover
-    const info = L.control()
+    this.info = L.control()
 
-    info.onAdd = () => {
-      info.div = L.DomUtil.create('div', 'info')
-      info.update()
-      return info.div
+    this.info.onAdd = () => {
+      this.info.div = L.DomUtil.create('div', 'info')
+      this.info.update()
+      return this.info.div
     }
 
-    info.update = (data) => {
-      info.div.innerHTML = `<h4>UK Constituency Map</h4>${(data ? `<b>${data.name}</b><br />${data.density} people / mi<sup>2</sup>` : 'Hover over a constituency')}`
+    this.info.update = (data) => {
+      this.info.div.innerHTML = `<h4>UK Constituency Map</h4>${(data ? `<b>${data.name}</b><br />` : 'Hover over a constituency')}`
     }
 
-    info.addTo(map)
-
-    // get color depending on value
-    const getColor = (d) => {
-      if (d > 1000) return '#800026'
-      if (d > 500) return '#BD0026'
-      if (d > 200) return '#E31A1C'
-      if (d > 100) return '#FC4E2A'
-      if (d > 50) return '#FD8D3C'
-      if (d > 20) return '#FEB24C'
-      if (d > 10) return '#FED976'
-      return '#FFEDA0'
-    }
-
-    const style = feature => ({
-      weight: 2,
-      opacity: 1,
-      color: 'white',
-      dashArray: '3',
-      fillOpacity: 0.7,
-      fillColor: getColor(feature.properties.density)
-    })
+    this.info.addTo(this.map)
 
     const highlightFeature = (e) => {
       const layer = e.target
@@ -68,18 +60,16 @@ class ElectionMap extends Component {
         layer.bringToFront()
       }
 
-      info.update(layer.feature.properties)
+      this.info.update(layer.feature.properties)
     }
 
-    let geojson
-
     const resetHighlight = (e) => {
-      geojson.resetStyle(e.target)
-      info.update()
+      this.geojson.resetStyle(e.target)
+      this.info.update()
     }
 
     const zoomToFeature = (e) => {
-      map.fitBounds(e.target.getBounds())
+      this.map.fitBounds(e.target.getBounds())
     }
 
     const onEachFeature = (feature, layer) => {
@@ -90,10 +80,19 @@ class ElectionMap extends Component {
       })
     }
 
-    geojson = L.geoJson(constituencyData, {
+    const style = feature => ({
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7,
+      fillColor: this.getColour(feature.properties.density)
+    })
+
+    this.geojson = L.geoJson(constituencyData, {
       style,
       onEachFeature
-    }).addTo(map)
+    }).addTo(this.map)
 
     const legend = L.control({ position: 'bottomright' })
 
@@ -101,22 +100,19 @@ class ElectionMap extends Component {
       const div = L.DomUtil.create('div', 'info legend')
       const grades = [ 0, 10, 20, 50, 100, 200, 500, 1000 ]
       const labels = []
-      let from
-      let to
 
       for (let i = 0; i < grades.length; i++) {
-        from = grades[i]
-        to = grades[i + 1]
+        const from = grades[i]
+        const to = grades[i + 1]
 
-        labels.push(`<i style="background:${getColor(from + 1)}"></i> ${from} ${(to ? `&ndash; ${to}` : '+')}`)
+        labels.push(`<i style="background:${this.getColour(from + 1)}"></i> ${from} ${(to ? `&ndash; ${to}` : '+')}`)
       }
 
       div.innerHTML = labels.join('<br>')
       return div
     }
 
-    legend.addTo(map)
-    this.rendered = true
+    legend.addTo(this.map)
   }
 
   render() {
