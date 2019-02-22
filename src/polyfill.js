c04ba0ff8bbfd5ee7135d0eb23cde6184a6883de
Jack Burgess <jackburgess124@gmail.com>
2019-02-22 16:03:24 +0000

    File cleanup

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,30 +1,9 @@
-/*
-* required polyfills
-*/
-
-/* IE9, IE10 and IE11 requires all of the following polyfills. */
-// import 'core-js/es6/symbol'
-// import 'core-js/es6/object'
-// import 'core-js/es6/function'
-// import 'core-js/es6/parse-int'
-// import 'core-js/es6/parse-float'
-// import 'core-js/es6/number'
-// import 'core-js/es6/math'
-// import 'core-js/es6/string'
-// import 'core-js/es6/date'
+import 'react-app-polyfill/ie9' // For IE 9-11 support
+import 'react-app-polyfill/ie11' // For IE 11 support
 import 'core-js/es6/array'
-// import 'core-js/es6/regexp'
 import 'core-js/es6/map'
-// import 'core-js/es6/weak-map'
 import 'core-js/es6/set'
 import 'core-js/es7/object'
-
-/* IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect'
-
-/* Evergreen browsers require these. */
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-// import 'core-js/es7/reflect'
 
 // CustomEvent() constructor functionality in IE9, IE10, IE11
 // eslint-disable-next-line
